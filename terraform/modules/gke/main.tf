758b3c4306447287e0cd25d0bbada7980c390eaf
Miroslav Tomic <kaosmonk@gmail.com>
2018-11-02 21:50:15 +0100

    Initial commit, gke module

---
+++ terraform/modules/gke/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  backend "gcs" {}
+}
+
+provider "google" {
+}
+
+data "google_container_registry_repository" "registry" {
+  project = "${var.project_id}"
+}
+
+data "google_container_engine_versions" "region" {
+  zone = "${var.zone}"
+  project = "${var.project_id}"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "gcs"
+  config {
+    bucket         = "${var.remote_state_bucket}"
+    prefix         = "vpc/terraform.tfstate"
+    region         = "${var.region}"
+    project        = "${var.project_id}"
+  }
+}
+
+resource "google_container_node_pool" "primary_pool" {
+  name       = "${var.name}-${var.env}-pool"
+  region       = "${var.region}"
+  node_count = "${var.node_count}"
+  cluster    = "${google_container_cluster.primary.name}"
+
+  node_config {
+    disk_size_gb    = "${var.disk_size}"
+    image_type      = "${var.image_type}"
+    machine_type    = "${var.machine_type}"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+      "https://www.googleapis.com/auth/pubsub",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = "${var.min_node_count}"
+    max_node_count = "${var.max_node_count}"
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name                     = "${var.name}-${var.env}-cluster"
+  region                     = "${var.region}"
+  network                  = "${data.terraform_remote_state.vpc.vpc_name}"
+  subnetwork               = "${data.terraform_remote_state.vpc.vpc_subnet_name}"
+  #additional_zones         = "${var.additional_zones}"
+  initial_node_count = 1
+  remove_default_node_pool = true
+  min_master_version       = "${data.google_container_engine_versions.region.latest_master_version}"
+  node_version             = "${data.google_container_engine_versions.region.latest_node_version}"
+
+  # Cluster labels
+  resource_labels = "${var.resource_labels}"
+
+  timeouts {
+    create = "30m"
+    delete = "30m"
+    update = "30m"
+  }
+}
---
+++ terraform/modules/gke/variables.tf
@@ -0,0 +1,61 @@
+variable "name" {
+  default = "kraken-k8s"
+}
+
+variable "env" {
+  default = "poc"
+}
+variable "node_count" {
+  default = 1
+}
+
+variable "disk_size" {
+  default = 10
+}
+
+variable "image_type" {
+  default = "COS"
+}
+
+variable "max_node_count" {
+  default = 3
+}
+
+variable "min_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
+
+variable "zone" {
+  default = "us-west2-a"
+}
+
+variable "additional_zones" {
+  default = [
+    "us-west2-b",
+    "us-west2-c",
+  ]
+}
+
+variable "resource_labels" {
+  default = {
+      environment = "poc"
+      cluster       = "kraken-poc-resource_label"
+      terraform   = "true"
+  }
+}
+
+variable "remote_state_bucket" {
+  default = "kraken-tf-state"
+}
+
+variable "region" {
+  default = "us-west2"
+}
+
+variable "project_id" {
+  default = "kraken-221319"
+}
